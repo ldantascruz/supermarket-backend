27655edf167b181c1aa5a92bbb5309e1391670c9
Lucas Dantas da Cruz <ldantascruz@gmail.com>
2024-05-10 22:56:16 -0300

    initial commit

---
+++ src/main/java/br/lcsoftware/supermarket/repositories/ProductRepository.java
@@ -0,0 +1,12 @@
+package br.lcsoftware.supermarket.repositories;
+
+import br.lcsoftware.supermarket.models.ProductModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface ProductRepository extends JpaRepository<ProductModel, UUID> {
+
+}
