e31fba399c97c797bb8aa910c8d206ab25553600
Lucas Dantas da Cruz <ldantascruz@gmail.com>
2024-05-27 20:28:24 -0300

    ignore tests

--- Dockerfile
+++ Dockerfile
@@ -5,7 +5,7 @@ RUN apt-get install openjdk-17-jdk -y
 COPY . .
 
 RUN apt-get install maven -y
-RUN mvn clean install -Dspring.profiles.active=test
+RUN mvn clean install -DskipTests
 
 FROM openjdk:17-jdk-slim
 
--- src/test/java/br/lcsoftware/supermarket/SupermarketApplicationTests.java
+++ src/test/java/br/lcsoftware/supermarket/SupermarketApplicationTests.java
@@ -3,9 +3,11 @@ package br.lcsoftware.supermarket;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
 @ActiveProfiles("test")
+@TestPropertySource(locations = "classpath:application-test.properties")
 class SupermarketApplicationTests {
 
 	@Test
